64f5d141d1709f99ad977484c7baa61ebfdf67b5
Kimsuhhee04 <kimyesol0428@gmail.com>
2026-03-26 22:29:25 +0900

    [섹션 2] 25. 스프링 데이터 JPA

---
+++ Seohui/src/test/java/com/example/lion14/Lion14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lion14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lion14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
