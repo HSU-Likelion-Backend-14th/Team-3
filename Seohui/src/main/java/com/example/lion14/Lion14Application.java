58b16f085811b592b8aa229df940ecaa490b4367
JongJin <166373147+ThreeeJ@users.noreply.github.com>
2026-03-29 18:25:20 +0900

    [1주차 과제] 김서희 (#6)
    * [섹션 2] 25. 스프링 데이터 JPA

---
+++ Seohui/src/main/java/com/example/lion14/Lion14Application.java
@@ -0,0 +1,13 @@
+package com.example.lion14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lion14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lion14Application.class, args);
+	}
+
+}
