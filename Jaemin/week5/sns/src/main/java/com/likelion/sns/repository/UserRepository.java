234f0e6043a9d7e6f9df4e9d23376b83a823f592
jmyoo0512 <jm030512@gmail.com>
2026-04-30 18:01:07 +0900

    feat: API 구현 + Swagger 문서 설정

---
+++ Jaemin/week5/sns/src/main/java/com/likelion/sns/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.likelion.sns.repository;
+
+import com.likelion.sns.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
