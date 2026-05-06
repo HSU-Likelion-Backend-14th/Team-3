155c2c7094edbe7977e30774ae8f14a1ee613f7e
Kimsuhhee04 <kimyesol0428@gmail.com>
2026-05-06 21:06:57 +0900

    feat: 공통 API 성공 및 에러 응답 클래스 구현

--- Suhhee/week6/demo/src/main/java/com/likelion/demo/global/response/BaseResponse.java
+++ Suhhee/week6/demo/src/main/java/com/likelion/demo/global/response/BaseResponse.java
@@ -1,4 +1,32 @@
-package com.likelion.demo.global.response.code;
+package com.likelion.demo.global.response;
 
+import com.likelion.demo.global.response.code.BaseResponseCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+// 성공 응답, 실패 응답 무관하게 공통으로 가지는 필드 관리
+@Getter
+@ToString
+@RequiredArgsConstructor
 public class BaseResponse {
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    private final String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+    public static BaseResponse of(Boolean isSuccess, BaseResponseCode baseResponseCode) {
+        return new BaseResponse(isSuccess, baseResponseCode.getCode(), baseResponseCode.getMessage());
+    }
+
+    public static BaseResponse of(Boolean isSuccess, BaseResponseCode baseResponseCode, String message) {
+        return new BaseResponse(isSuccess, baseResponseCode.getCode(), message);
+    }
+
+    public static BaseResponse of(Boolean isSuccess, String code, String message) {
+        return new BaseResponse(isSuccess, code, message);
+    }
 }
